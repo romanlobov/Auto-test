78e9f16047b758eec38b336c804e9257c659d468
Роман <roman.lobov@eko-point.ru>
2018-09-06 09:49:15 +0300

    11
    111

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -79,10 +79,6 @@ namespace WindowsFormsApplication1
             string[] liteserv = { "chcp 855", "cd C:/1/Тестирование/feeding-client", "lite-server" };
             File.WriteAllLines("coms8.bat", liteserv, Encoding.GetEncoding(855));
             Process.Start("coms8.bat");
-
-            string[] Tasks = { "chcp 855", "C:/1/Тестирование/feeding-task/netcoreapp2.0/win7-x64/publish/Ekopoint.Feeding.Tasks.exe" };
-            File.WriteAllLines("coms7.bat", Tasks, Encoding.GetEncoding(855));
-            Process.Start("coms7.bat");
-        }
+                  }
     }
 }
